de629eae8975a20117b09a4d5ff593619bfc56a8
chris <chris.kolann@gmail.com>
2025-02-04 11:58:00 +0100

    wezterm window is bigger

--- dot_wezterm.lua
+++ dot_wezterm.lua
@@ -1,18 +1,14 @@
 -- Pull in the wezterm API
 local wezterm = require 'wezterm'
-
--- This will hold the configuration.
 local config = wezterm.config_builder()
 
--- This is where you actually apply your config choices
-
--- For example, changing the color scheme:
 config.color_scheme = 'AdventureTime'
 config.font = wezterm.font("FiraCode Nerd Font")
 config.font_size = 10
 config.default_cursor_style = "BlinkingBlock"	-- or "BlinkingBar" "BlinkingUnderline"
 config.enable_tab_bar = false
 config.window_background_opacity = 0.95
+config.initial_cols = 167
+config.initial_rows = 47
 
--- and finally, return the configuration to wezterm
 return config
